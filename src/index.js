0f7c3c6c44e0c5de1dbc09463d1f864c2a20c1f5
Mike Alizade <michael.alizade@pearson.com>
2018-06-01 17:45:36 +0100

    codeship test 4

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ sagaMiddleware.run(watchFetchData);
 // production build and deployment (codeship?)
 // expanding app - users / login / favourites etc
 
-// codeship test 3
+// codeship test 4
 
 ReactDOM.render(
   <Provider store={store}>
